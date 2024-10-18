78cf61b629c2d273412f356cd53c00a3d5a7d3a9
Edwin <lgedwin.martinez@gmail.com>
2024-10-17 20:51:32 -0500

    inicio de funcion con createElemnt

--- controllers/controllerTiempos.js
+++ controllers/controllerTiempos.js
@@ -1,10 +1,31 @@
+import { Tiempo } from "../models/modelTiempos.js"
+
+
 export function registrarTiempo() {
     let fecha = document.getElementById('fecha').value
     let tiempo = document.getElementById('tiempo').value
     let proyecto = document.getElementById('proyecto').value // VALUE para almacenar la informcion de un Input
-    console.log(fecha)
-    console.log(tiempo)
-    console.log(proyecto)
-    console.log('Registrando el Tiempo')
 
+    let newRegistro = {
+        fecha: fecha,
+        tiempo: tiempo,
+        proyecto: proyecto
+    }
+    Tiempo.push(newRegistro)
+    console.log(Tiempo)
+    crearRegistroTiempo()
+
+}
+function crearRegistroTiempo() {
+    let registro = document.createElement('tr')
+    let fecha = document.createElement('td')
+    let tiempo = document.createElement('td')
+    let proyecto = document.createElement('td')
+    let acciones = document.createElement('td')
+    let editar = document.createElement('button')
+    let eliminar = document.createElement('button')
+
+    acciones.append(editar, eliminar)
+    registro.append(fecha, tiempo, proyecto, acciones)
+    console.log(registro)
 }
