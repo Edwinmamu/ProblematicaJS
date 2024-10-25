36a1b82ebdd641d992a34be2247af3fe258e448a
Edwin <lgedwin.martinez@gmail.com>
2024-10-24 20:37:13 -0500

    se crea estructura basica de vista de viewProyectos

---
+++ controllers/controllerProyecto.js
@@ -0,0 +1,13 @@
+import { proyectos } from "../models/modelProyecto.js";
+
+export function listarProyectos (){
+
+}
+
+export function filtrarPorEmpresa(){
+
+}
+export function filtrarPorProyecto(){
+
+}
+export function filtrarPorTipoProyecto(){}
